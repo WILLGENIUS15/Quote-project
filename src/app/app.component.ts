18b31b3f8aaef3b42c02b696716f1b95a259a356
fidel omondi <willgenius@gmail.com>
2019-08-19 14:11:52 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Quote } from './quote'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  quotes = [
+    new Quote(1, 'The sun is a star.','A.B Clark',"Z.G"),
+    new Quote(2, 'The moon is beautiful.', 'B.C Smith',"E.I"),
+    new Quote(3, 'The earth is meh.', 'D.E Jacobsons',"T.O"),
+    new Quote(4, 'The galaxy is large.', 'F.G Randos',"P.S"),
+    new Quote(5, 'The universe is expanding.', 'I.J Persons',"Q.T"),
+];
+  constructor(){  }
+}
---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+ 
+  quotes = [
+    new Quote(1, 'The sun is a star.','A.B Clark',"Z.G"),
+    new Quote(2, 'The moon is beautiful.', 'B.C Smith',"E.I"),
+    new Quote(3, 'The earth is meh.', 'D.E Jacobsons',"T.O"),
+    new Quote(4, 'The galaxy is large.', 'F.G Randos',"P.S"),
+    new Quote(5, 'The universe is expanding.', 'I.J Persons',"Q.T"),
+];
+
+addNewQuote(quote){
+  let quoteLength = this.quotes.length;
+  quote.id=quoteLength+1;
+  this.quotes.unshift(quote)
+
+}
+
+toogleDetails(index){
+  this.quotes[index].showAuthor = !this.quotes[index].showAuthor;
+}
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+
